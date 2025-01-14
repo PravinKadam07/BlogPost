99fe7aa625879ad381f3acf81e67beaa4af0fc75
Pravin Kadam <pravin.kadam@jcilpune.com>
2025-01-14 15:11:14 +0530

    blogPost project setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import Home from './Home';
+const App = () => {
+  return (
+    <div className='w-full h-[100vh]'>
+     <Home></Home>
+    </div>
+  );
+};
+
+export default App;
+
